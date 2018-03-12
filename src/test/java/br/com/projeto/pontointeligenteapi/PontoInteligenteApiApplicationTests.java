8a53172e3a87e264b9c27d18522fc284eab76e58
Raphael Maquiné <raphael.maquine@prodam.am.gov.br>
2018-03-12 14:30:44 -0400

    Adicionado configuraçao para executar o teste corretamente no travis

--- src/test/java/br/com/projeto/pontointeligenteapi/PontoInteligenteApiApplicationTests.java
+++ src/test/java/br/com/projeto/pontointeligenteapi/PontoInteligenteApiApplicationTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.projeto.pontointeligente.api.PontoInteligenteApiApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PontoInteligenteApiApplication.class)
 public class PontoInteligenteApiApplicationTests {
 
 	@Test
