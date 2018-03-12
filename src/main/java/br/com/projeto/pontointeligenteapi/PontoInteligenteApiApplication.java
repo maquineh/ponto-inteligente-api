72d1052c55551ef068dc39dc27a521fce94d0d06
Raphael Maquine <rapiner@gmail.com>
2018-03-11 22:34:34 -0400

    Commit inicial dos arquivos do projeto.

---
+++ src/main/java/br/com/projeto/pontointeligenteapi/PontoInteligenteApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.projeto.pontointeligenteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApiApplication.class, args);
+	}
+}
