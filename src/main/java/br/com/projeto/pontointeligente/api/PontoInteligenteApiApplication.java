8414cf502060135f4e05040dd061c0c70e0906f9
Raphael Maquine <rapiner@gmail.com>
2018-03-12 01:08:16 -0400

    Adicionado as classes de entidade do projeto e os tipos de Enum

--- src/main/java/br/com/projeto/pontointeligente/api/PontoInteligenteApiApplication.java
+++ src/main/java/br/com/projeto/pontointeligente/api/PontoInteligenteApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.projeto.pontointeligenteapi;
+package br.com.projeto.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
